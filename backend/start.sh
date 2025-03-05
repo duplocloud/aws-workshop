18f7c660c26cba2942fa641844bc05efff6fc38e
Andy Boutte <andy@duplocloud.net>
2025-03-05 13:06:00 -0500

    new branch with just Dockerfile

--- backend/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd /app
-source venv/bin/activate
-# source .env
-nohup gunicorn --bind 0.0.0.0:5000 --workers 4 server:app > gunicorn.log 2>&1 &
-nohup nginx -g 'daemon off;' >> gunicorn.log 2>&1 &
-tail -f gunicorn.log
